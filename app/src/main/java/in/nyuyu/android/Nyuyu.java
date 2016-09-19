ffedac2c122e5f758db760ec5da6deb51ddd46c6
Vinay B.N <vinay@auom.co.in>
2016-09-19 23:59:25 +0530

    mend

--- app/src/main/java/in/nyuyu/android/Nyuyu.java
+++ app/src/main/java/in/nyuyu/android/Nyuyu.java
@@ -2,8 +2,18 @@ package in.nyuyu.android;
 
 import android.app.Application;
 
+import jonathanfinerty.once.Once;
+import timber.log.Timber;
+
 /**
  * Created by Vinay on 19/09/16.
  */
 public class Nyuyu extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+        Once.initialise(this);
+    }
 }
--- app/src/main/java/in/nyuyu/android/splash/SplashActivity.java
+++ app/src/main/java/in/nyuyu/android/splash/SplashActivity.java
@@ -1,15 +1,56 @@
 package in.nyuyu.android.splash;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 
+import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
 import in.nyuyu.android.R;
+import in.nyuyu.android.commons.NyuyuActivity;
+import in.nyuyu.android.commons.Rx;
+import in.nyuyu.android.commons.Scopes;
+import in.nyuyu.android.onboarding.OnboardingActivity;
+import in.nyuyu.android.style.StyleListActivity;
+import jonathanfinerty.once.Once;
+import rx.Single;
+import timber.log.Timber;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends NyuyuActivity {
+
+    FirebaseAuth firebaseAuth;
+    @BindView(R.id.activity_splash) CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        firebaseAuth = FirebaseAuth.getInstance();
+        Rx.user(firebaseAuth)
+                .flatMap(user -> user == null ? Rx.signInAnonymously(firebaseAuth) : Single.just(user))
+                .map(FirebaseUser::getUid)
+                .subscribe(
+                        userId -> {
+                            if (Once.beenDone(Once.THIS_APP_INSTALL, Scopes.onboarding(userId))) {
+                                startActivity(new Intent(this, StyleListActivity.class));
+                            } else {
+                                startActivity(new Intent(this, OnboardingActivity.class));
+                            }
+                            finish();
+                        },
+                        throwable -> {
+                            if (throwable.getClass().equals(FirebaseNetworkException.class)) {
+                                setContentView(R.layout.activity_splash);
+                                Snackbar.make(coordinatorLayout, "You are offline x_x", Snackbar.LENGTH_INDEFINITE)
+                                        .setAction("Retry", v -> recreate())
+                                        .show();
+                            } else {
+                                Timber.e(throwable, throwable.getMessage());
+                            }
+
+                        });
     }
 }
