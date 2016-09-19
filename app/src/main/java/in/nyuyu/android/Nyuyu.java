a4ca8d2ba86d71e8bfd2b35fb38a7081ab22c2bb
Vinay B.N <vinay@auom.co.in>
2016-09-19 19:13:17 +0530

    Initial commit

---
+++ app/src/main/java/in/nyuyu/android/Nyuyu.java
@@ -0,0 +1,9 @@
+package in.nyuyu.android;
+
+import android.app.Application;
+
+/**
+ * Created by Vinay on 19/09/16.
+ */
+public class Nyuyu extends Application {
+}
---
+++ app/src/main/java/in/nyuyu/android/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package in.nyuyu.android.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.nyuyu.android.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
